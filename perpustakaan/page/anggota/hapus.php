a86e350366762410518311922c6540c2b1b4b19f
davitcanyagho <114200606+davitcanyagho@users.noreply.github.com>
2022-10-05 05:32:57 +0700

    Delete perpustakaan directory

--- perpustakaan/page/anggota/hapus.php
+++
@@ -1,8 +0,0 @@
-<?php
-    $id = $_GET['id'];
-    $koneksi->query("DELETE FROM anggota WHERE nim = '$id'");
-
-
-echo "<script>window.location.href='?page=anggota';</script>";
-
-?>
--- perpustakaan/page/transaksi/hapus.php
+++
@@ -1,8 +0,0 @@
-<?php
-    $id = $_GET['id'];
-    $koneksi->query("DELETE FROM transaksi WHERE id = '$id'");
-?>
-
-<script type="text/javascript">
-    window.location.href="?page=transaksi";
-</script>
